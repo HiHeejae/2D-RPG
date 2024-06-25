3e83dac6472549736d373be3d6ca8993d7d9e409
Hiheejae <aag391345@gmail.com>
2024-06-25 12:14:52 +0900

    TownScene 로드

--- Assets/1. Scripts/NPC.cs
+++ Assets/1. Scripts/NPC.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NPC : MonoBehaviour
 {
+    public GameObject DialogueUI;
+
     private GameObject playerObj;
     private float distance;
 
+    public GameObject[] DialogueTextObj;
+    private int dIndex = 0;
 
     private void Update()
     {
@@ -15,5 +20,29 @@ public class NPC : MonoBehaviour
 
         distance = Vector2.Distance(transform.position, playerObj.transform.position);
         Debug.Log($"distance : {distance}");
+
+        if (distance <= 3)
+            DialogueUI.SetActive(true);
+        else
+            DialogueUI.SetActive(false);
+    }
+
+    public void NextBtn(string name)
+    {
+        DialogueTextObj[dIndex].SetActive(false);
+        if (name == "Next")
+        {
+            if (dIndex < DialogueTextObj.Length - 1) dIndex++;
+        }
+        else if (name == "Prev")
+        {
+            if (dIndex > 0) dIndex--;
+        }
+        DialogueTextObj[dIndex].SetActive(true);
+    }
+
+    public void TownBtn()
+    {
+        SceneManager.LoadScene("TownScene");
     }
 }
